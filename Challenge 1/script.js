0ac154e3c910aeeb6b6a43654cf941daa426053a
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-06 15:05:30 +0200

    Update: Completed challenge 1

---
+++ Challenge 1/script.js
@@ -0,0 +1,45 @@
+// Note that the new objects should have a nested address object that contains all address-specific information.
+// Note that all values in the object should be reassigned from the variables at the top of the file.
+// The only exception is the new age value that is added to the object.
+// In the latter, we assign a value directly when declaring the object.
+
+const leoName = 'Leo Musvaire'
+const leoNumber = '2'
+const leoStreet = 'Church St.'
+const leoPostal = '3105'
+const leoBalance = '-10'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+const sarahNumber = '13'
+const sarahStreet = 'William Close'
+const sarahPostal = '0310'
+
+// Only change below this line
+
+const leo = {
+	name: leoName,
+	balance: parseInt(leoBalance),
+	access_id: "47afb389-8014-4d0b-aff3-e40203d2107f",
+	age: 24,
+	address: {
+		number: leoNumber,
+		street: leoStreet,
+		"postal-code": leoPostal
+	}
+}
+
+const sarah = {
+	name: sarahName + sarahSurname,
+	age: 62,
+	access_id: "6b279ae5-5657-4240-80e9-23f6b635f7a8",
+	balance: sarahBalance,
+	address: {
+		number: sarahNumber,
+		street: sarahStreet,
+		"postal-code": sarahPostal
+	}
+}
+console.log(leo, leo.address["postal-code"])
+console.log(sarah, sarah.address["postal-code"])
